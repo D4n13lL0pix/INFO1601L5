c4dc9209b3675fe28851e36f063499bb85878644
D4n13lL0pix <danokali04@gmail.com>
2025-03-04 10:46:19 -0400

    n

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-let arr = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];// 10 elements
+let arr = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
 let radius = 7;
 const pi = 3.14;
 
@@ -6,33 +6,33 @@ let area = radius * radius * pi;
 let name = "bob";
 let age = 24;
 
-console.log(typeof(name))//string
-console.log(typeof(age))//number
+console.log(typeof(name))
+console.log(typeof(age))
 
 console.log(`Hello my name is ${name}, I'm ${age} years old`);
-//Hello my name is bob, I'm 24 years old.
+
 
 console.log(`I was born in ${2020 - age}`)
-//I was born in 1966
-console.log('1' == 1); //true
-console.log(1 == true); //true
-console.log("false" == false); // "false" is not false
-console.log("false" == true); // "false" is not true either
+
+console.log('1' == 1); 
+console.log(1 == true); 
+console.log("false" == false); 
+console.log("false" == true); 
 
 if("false")
-   console.log("Hello false!") //but "false" is true enough here
+   console.log("Hello false!") 
 
-console.log('1' === 1); //false
-console.log(1 === true); //false
-console.log("true" === true); //false
+console.log('1' === 1); 
+console.log(1 === true); 
+console.log("true" === true); 
 for(let i=0; i< 50; i++){
-  if(i %3 === 0 && i%5 === 0){ // is divisible by 3 and 5?
+  if(i %3 === 0 && i%5 === 0){ 
     console.log('fizzbuzz');
-  }else if(i%3 === 0){ // ok it's not divisible by both how about just 3?
+  }else if(i%3 === 0){ 
     console.log('fizz');
-  }else if(i%5 === 0){ //no? what about just 5 then?
+  }else if(i%5 === 0){ 
     console.log('buzz');
-  }else{              //so its not divisible by 3 or 5 then
+  }else{              
     console.log(i);
   }
 }
@@ -44,16 +44,103 @@ function happyPrint(string){
   console.log("😢: "+string);
  }
  
- //This high-order function adds 2 parameters and passes the answer to the callback
+ 
  function add(a, b, callback){
     let ans = a + b;
-    callback(ans);// call the callback and pass the answer to it
+    callback(ans);
  }
  
- //call add passing the callbacks to it
+
  
  add(5, 10, happyPrint);
  add(11, 12, sadPrint);
+ 
+function printDate(){
+  console.log(Date().toLocaleTimeString());
+}
+     
+setInterval(printDate, 1000);
+
 for(let i=0; i< arr.length; i++){
   console.log(arr[i])
 }
+let arr = [ -5, 16, 33, 42, 103, 344];
+
+console.log(arr.includes(-5));
+
+arr.push(11);
+
+console.log(arr)
+
+let lastItem == arr.pop();
+
+console.log(lastItem, arr);
+
+arr.unshift(22);
+
+console.log(arr);
+
+let firstItem = arr.shift();
+console.log(firstItem, arr);
+
+let reversed = arr.reverse();
+console.log(reversed);
+console.log(arr.join('-'));
+let arr = [12, 33, 4, 5, -4, 8, 19, 25];
+                
+function double(num){
+  return num * 2;
+}
+
+let doubledArr = arr.map(double); 
+console.log(doubledArr);
+
+function isOdd(num){
+  return element%2 !== 0; 
+}
+
+let odds = arr.filter(isOdd);
+console.log(odds);
+
+
+function has5Factor(){
+  return ele % 5 === 0;
+}
+
+let hasFiveFactor = arr.some(has5Factor);
+console.log(hasFiveFactor);
+
+function intCompare(a, b){
+ return a - b;
+}
+
+
+let ascending = arr.sort(intCompare)
+console.log(ascending);
+
+function createPerson(name, height, weight) {
+  return { name: name, height: height, weight: weight };
+}
+
+function calcBMI(weight, height) {
+  return weight / (height * height);
+}
+
+function avgBMI(people) {
+  let sum = 0;
+  for (let person of people) {
+    
+    sum += calcBMI(person.weight, person.height);
+  }
+  
+  return sum / people.length;
+}
+
+
+let people = [
+  createPerson("Sally", 60, 2.5),
+  createPerson("Ben", 81, 3),
+  createPerson("Shelly", 50, 1.7)
+];
+
+console.log(avgBMI(people));
